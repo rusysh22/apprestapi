8e17cbaa96b9dafb6b99dcd69159c70badd7771f
rusy <rusydani.sh@gmail.com>
2022-08-31 15:44:11 +0700

    setting routes

--- server.js
+++ server.js
@@ -6,6 +6,11 @@ const app = express();
 //parse app/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+//panggil routes
+var routes = require('./routes');
+routes(app);
+
 app.listen(8000, () => {
     console.log(`Server started on port`);
 });
