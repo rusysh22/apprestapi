e0ba37831a4fca2e0a72c3fb81e0e96e57cf83dd
rusy <rusydani.sh@gmail.com>
2022-08-31 16:04:36 +0700

    update data in controller and router

--- controller.js
+++ controller.js
@@ -6,3 +6,15 @@ var connection = require('./koneksi');
 exports.index = function(req, res){
     response.ok("Aplikasi REST API sudah OK!", res)
 };
+
+//Call Data
+
+exports.tampilsemua = function(req,res){
+    connection.query('SELECT * FROM mahasiswa', function(error, rows, fields){
+        if(error){
+            connection.log(error);
+        }else {
+            response.ok(rows, res)
+        }
+    });
+};
